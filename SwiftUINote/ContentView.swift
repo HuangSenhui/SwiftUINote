1689bd21a5bdc268021012fecdfeabdb95c9db1d
HuangSenhui <huangsenhui@live.com>
2020-12-22 21:49:57 +0800

    Initial Commit

---
+++ SwiftUINote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINote
+//
+//  Created by HuangSenhui on 2020/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUINote/SwiftUINoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINoteApp.swift
+//  SwiftUINote
+//
+//  Created by HuangSenhui on 2020/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
